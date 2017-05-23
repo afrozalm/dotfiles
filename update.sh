642fa829a17195804b406613ad1679721c93aca0
afrozalm <alam.afroz011@gmail.com>
2017-05-23 10:13:57 -0700

    added check conditions directories

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp init.el ~/.emacs.d/
-cp notifyosd.zsh ~/.zsh/
+[ -d "$HOME/.emacs.d" ] && cp init.el ~/.emacs.d/ || echo "$HOME/.emacs.d does not exist"
+[ -d "$HOME/.zsh" ] && cp notifyosd.zsh ~/.zsh/ || echo "$HOME/ does not exist"
+[ -d "$HOME/.config/zathura" ] && cp zathurarc ~/.config/zathura/ || echo "$HOME/.config/zathura/ does not exist"
+[ -d "$HOME/.oh-my-zsh/themes" ] && cp agnosterAfroLight.zsh-theme ~/.oh-my-zsh/themes/ || echo "$HOME/.oh-my-zsh/themes does not exist"
 cp .tmux.conf  ~/
 cp .vimperatorrc  ~/
 cp .vimrc  ~/
 cp .vimrc.local  ~/
 cp .zshrc  ~/
-cp zathurarc ~/.config/zathura/
-cp agnosterAfroLight.zsh-theme ~/.oh-my-zsh/themes/
