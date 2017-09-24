8e259b793263f7d9d2a90641caf5e013cfd9b899
afrozalm <alam.afroz011@gmail.com>
2017-09-24 18:09:45 +0530

    global yas

--- init.el
+++ init.el
@@ -26,6 +26,11 @@
 
 
 ;; kbd for yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
+
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand)
